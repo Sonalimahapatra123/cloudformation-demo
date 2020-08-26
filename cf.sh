928b95870ed6d7d95fa39339a36d1c25f13f437d
Sonalimahapatra123 <45412203+Sonalimahapatra123@users.noreply.github.com>
2020-08-26 16:28:17 +0530

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -6,7 +6,7 @@
             REGION= $us-east-1
             #$REGION
             type_formation=""
-            if ! aws cloudformation describe-stacks --stack-name '$STACK_NAME' ; then
+            if ! aws cloudformation describe-stacks --stack-name $STACK_NAME ; then
             aws cloudformation create-stack --stack-name x3bucket --template-body file://simplests3cft.json --region $REGION
             else
               type_formation='update-stack'
