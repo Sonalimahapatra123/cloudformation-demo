d829c1b2b8ccdeeaa69b9d67c5fe3f4e6740480c
Sonalimahapatra123 <45412203+Sonalimahapatra123@users.noreply.github.com>
2020-08-26 12:13:01 +0530

    Create cf.sh
    New File Added

---
+++ cf.sh
@@ -0,0 +1,14 @@
+            #!/bin/bash
+            #set -x
+            sh "aws cloudformation get-template --stack-name s3bucket"
+            stack_name="s3bucket"
+            //$STACK_NAME
+            region="us-east-1"
+            //$REGION
+            type_formation=""
+            if ! aws cloudformation describe-stacks --stack-name '$stack_name' ; then'
+            sh "aws cloudformation create-stack --stack-name x3bucket --template-body file://simplests3cft.json --region 'us-east-1'"
+            else
+              type_formation='update-stack'
+            fi
+            done
