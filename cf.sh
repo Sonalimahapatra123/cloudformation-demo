3bcf5b03220f67e4e66b567190264b4c850e015f
Sonalimahapatra123 <45412203+Sonalimahapatra123@users.noreply.github.com>
2020-08-26 17:13:16 +0530

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,14 +1,71 @@
-            #!/bin/bash
-            #set -x
-            #sh "aws cloudformation get-template --stack-name s3bucket"
-            STACK_NAME= $s3bucket
-            #$STACK_NAME
-            REGION= $us-east-1
-            #$REGION
-            type_formation=""
-            if ! aws cloudformation describe-stacks --stack-name $STACK_NAME ; then
-            aws cloudformation create-stack --stack-name x3bucket --template-body file://simplests3cft.json --region $REGION
-            else
-              type_formation='update-stack'
-            fi
-            echo "Success"
+#!/usr/bin/env bash
+
+usage="Usage: $(basename "$0") region stack-name [aws-cli-opts]
+where:
+  region       - the AWS region
+  stack-name   - the stack name
+  aws-cli-opts - extra options passed directly to create-stack/update-stack
+"
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "help" ] || [ "$1" == "usage" ] ; then
+  echo "$usage"
+  exit -1
+fi
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
+  echo "$usage"
+  exit -1
+fi
+
+shopt -s failglob
+set -eu -o pipefail
+
+echo "Checking if stack exists ..."
+
+if ! aws cloudformation describe-stacks --region $1 --stack-name $2 ; then
+
+  echo -e "\nStack does not exist, creating ..."
+  aws cloudformation create-stack \
+    --region $1 \
+    --stack-name $2 \
+    ${@:3}
+
+  echo "Waiting for stack to be created ..."
+  aws cloudformation wait stack-create-complete \
+    --region $1 \
+    --stack-name $2 \
+
+else
+
+  echo -e "\nStack exists, attempting update ..."
+
+  set +e
+  update_output=$( aws cloudformation update-stack \
+    --region $1 \
+    --stack-name $2 \
+    ${@:3}  2>&1)
+  status=$?
+  set -e
+
+  echo "$update_output"
+
+  if [ $status -ne 0 ] ; then
+
+    # Don't fail for no-op update
+    if [[ $update_output == *"ValidationError"* && $update_output == *"No updates"* ]] ; then
+      echo -e "\nFinished create/update - no updates to be performed"
+      exit 0
+    else
+      exit $status
+    fi
+
+  fi
+
+  echo "Waiting for stack update to complete ..."
+  aws cloudformation wait stack-update-complete \
+    --region $1 \
+    --stack-name $2 \
+
+fi
+
+echo "Finished create/update successfully!"
